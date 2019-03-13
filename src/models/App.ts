66093d2f68ecfa9f66e4455cb98789e37bbf065c
Matthew Dooley <Matthew@Dooley.xyz>
2019-03-13 00:58:08 -0500

    Update App model

--- src/models/App.ts
+++ src/models/App.ts
@@ -1,16 +1,51 @@
 import {Model} from "../Model";
+import {ModelInterface, ModelInterfaceStatic, staticImplements} from "../interfaces/ModelInterface";
 
-// @ts-ignore
-export class App extends Model {
-    static model_name: string;
+@staticImplements<ModelInterfaceStatic>()
+export class App extends Model implements ModelInterface {
+    type: ModelInterfaceStatic = App;
+    static model_name: string = 'app';
+
     static booted: boolean = false;
 
     constructor(params?: any) {
         super(params);
-        (App.booted || App.boot())
+        (this.type.booted || this.type.boot())
     }
 
-    static boot(): void {
-        this.model_name = 'app';
+    static boot() {
+        this.booted = true;
+    }
+
+    get(name: string) {
+        return super.get(name);
+    }
+
+    set(name: string, value: any) {
+        return super.set(name, value);
+    }
+
+    static all() {
+        return super.all(this.model_name);
+    }
+
+    find(id: string) {
+        return super.find(this.type.model_name, id);
+    }
+
+    add() {
+        return super.add(this.type.model_name);
+    }
+
+    edit(id: string) {
+        return super.edit(this.type.model_name, id);
+    }
+
+    delete(id: string) {
+        return super.delete(this.type.model_name, id);
+    }
+
+    form() {
+        return super.form(this.type.model_name);
     }
 }
