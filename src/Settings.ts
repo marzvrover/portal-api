35df1d78218c558baea05784fb4f629fa4014491
Matthew Dooley <Matthew@Dooley.xyz>
2019-03-13 00:56:28 -0500

    Turn off debug.
    Turn debug off for ease of TDD

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,3 +1,3 @@
 export const VERSION: string = "v1";
 export const URL: string = window.location.protocol + "//" + window.location.host;
-export const DEBUG: boolean = true;
+export const DEBUG: boolean = false;
