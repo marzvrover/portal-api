b5cd3edd678d7a5274dfd953d264c6d2384ae1c8
Matthew Dooley <Matthew@Dooley.xyz>
2019-03-13 00:59:36 -0500

    Update tests

--- src/Model.spec.ts
+++ src/Model.spec.ts
@@ -1,12 +1,32 @@
 import {Portal} from "./Portal";
 import {App} from "./models/App";
+import pluralize = require("pluralize");
 
-describe('App', () => {
-    test('Model is booted', () => {
-        expect(Portal.App.model_name).toBe('app');
-    });
+const MODELS = [
+  App,
+];
 
-    test('Model can access API', () => {
-        App.list();
+for (let key of MODELS) {
+    describe(Portal.ucfirst(key.model_name), () => {
+        test('Model will boot', () => {
+            new key(); // should boot
+            expect(Portal.App.booted).toBe(true);
+        });
+
+        test('Model can access API', () => {
+            return key.all().then((response) => {
+                expect(response.hasOwnProperty(pluralize(key.model_name))).toBe(true);
+            });
+        });
+
+        test('Model can set and get attributes', () => {
+            const model = new key();
+            const attribute = 'name';
+            const value = "Gmail";
+
+            model.set(attribute, value);
+
+            expect(model.get(attribute)).toBe(value);
+        });
     });
-});
+}
