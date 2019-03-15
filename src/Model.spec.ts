62011e7cfe7c49723cf14dc644fdeda45194e9cb
Matthew Dooley <Matthew@Dooley.xyz>
2019-03-15 02:28:46 -0500

    Update/Add tests

--- src/Model.spec.ts
+++ src/Model.spec.ts
@@ -1,32 +1,60 @@
 import {Portal} from "./Portal";
 import {App} from "./models/App";
-import pluralize = require("pluralize");
 
 const MODELS = [
   App,
 ];
 
-for (let key of MODELS) {
-    describe(Portal.ucfirst(key.model_name), () => {
+for (let model of MODELS) {
+    describe(Portal.ucfirst(model.model_name), () => {
         test('Model will boot', () => {
-            new key(); // should boot
+            new model(); // should boot
             expect(Portal.App.booted).toBe(true);
         });
 
         test('Model can access API', () => {
-            return key.all().then((response) => {
-                expect(response.hasOwnProperty(pluralize(key.model_name))).toBe(true);
+            return model.all().then((response) => {
+                expect(response).toBeDefined();
             });
         });
 
         test('Model can set and get attributes', () => {
-            const model = new key();
+            const tmp_model = new model();
             const attribute = 'name';
             const value = "Gmail";
 
-            model.set(attribute, value);
+            tmp_model.set(attribute, value);
 
-            expect(model.get(attribute)).toBe(value);
+            expect(tmp_model.get(attribute)).toBe(value);
+        });
+
+        test('`all()` returns a promise with list of model instances', () => {
+           return model.all().then((response) => {
+               expect(response[0]).toBeInstanceOf(model);
+           });
+        });
+
+        test('`find(slug)` returns a promise with a model instance', () => {
+            return model.all().then(async (models) => {
+                let slug = models[0].get('slug');
+                return model.find(slug).then((tmp_model) => {
+                    expect(tmp_model.get('slug')).toBe(slug);
+                });
+            });
+        });
+
+        test('`create(attributes)` returns a promise with a model ' +
+            'instance that has been saved to the server', () => {
+
+            if (model.model_name == App.model_name) {
+                return model.create({
+                    'name': 'test-name',
+                    'url': 'https://test.url',
+                    'iconPath': 'testing/path'
+                }).then((response) => {
+                    expect(response.get('slug')).toBeDefined();
+                });
+            }
         });
     });
 }
